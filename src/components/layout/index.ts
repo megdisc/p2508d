40d367a70d290f39dbbbad27e147fc179bf96e43
megdisc <megdisc@gmail.com>
2025-10-20 17:40:25 +0900

    251020-17-2

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,9 +1,10 @@
-// src/components/layout/index.ts
-export * from './Header';
-export * from './Navigation';
-export * from './PageHeader';
-export * from './ListPageLayout';
+export * from './CardPageLayout';
 export * from './DetailPageLayout';
 export * from './FormActionsLayout';
 export * from './FormGrid';
-export * from './CardPageLayout'; // ★ 追加
+export * from './Header';
+export * from './ListPageLayout';
+export * from './Navigation';
+export * from './PageHeader';
+export * from './PageLayout'; // 追加
+export * from './SectionPageLayout'; // 追加
